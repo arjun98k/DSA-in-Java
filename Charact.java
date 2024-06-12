4496928cbe5eaa279115417df6498163e8b5ed73
arjun98k <arjunkandekar14@gmail.com>
2024-06-12 12:54:44 +0530

    first commit

---
+++ Charact.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Charact {
+    public static void main(String[] args) {
+       Scanner ak = new Scanner(System.in);
+       System.out.println("enter the characters");
+       String aksh = ak.next();
+       if ("a" .equals(aksh) ) {
+        System.out.println("you are arjun");
+       } else {
+        System.out.println("you are akash");
+       }
+
+    }
+}
