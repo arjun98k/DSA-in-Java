9c6a8553853e78c2e667701f2e7bd3f44927a924
arjun98k <arjunkandekar14@gmail.com>
2024-06-13 10:05:50 +0530

    kuch likha hai

---
+++ Increment_decrement.java
@@ -0,0 +1,11 @@
+public class Increment_decrement
+{
+	public static void main(String[] args){
+		int x = 11;
+		int y = 11;
+		System.out.println(x++);
+		System.out.println(x);
+		System.out.println(++y);
+		System.out.println(x*y);
+	}
+}
