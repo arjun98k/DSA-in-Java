1238d7b3590d6df31b4822a8a1ac95543a28e45d
arjun98k <arjunkandekar14@gmail.com>
2024-06-15 00:43:46 +0530

    first commit

---
+++ Left_right_shift.java
@@ -0,0 +1,10 @@
+public class Left_right_shift {
+    public static void main(String[] args) {
+
+        // left shit operator 2 raised to 3 == 2**3 = 10 * 2*2*2 = 80
+        System.out.println(10<<3);
+
+        // right shift operator 2 raised to 3 == 2**3 / 2*2*2 = 1
+        System.out.println(10>>3);
+    }
+}
