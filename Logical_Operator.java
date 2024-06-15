0c9d14754e2de71b9c93054fa7884a6039c6b00c
arjun98k <arjunkandekar14@gmail.com>
2024-06-15 14:11:19 +0530

    first commit

--- Logical_Operator.java
+++ Logical_Operator.java
@@ -1,8 +1,21 @@
 public class Logical_Operator {
     public static void main(String[] args) {
-        boolean a = true;
-        boolean b = true;
-        boolean answ = a && b ;
-        System.out.println(answ);
-    }
+        // Logical_Operator && and operator
+
+    //     boolean a = true;
+    //     boolean b = true;
+    //     boolean answ = a && b ;
+    //     System.out.println(answ);
+
+        // logical Or operator 
+        // boolean a = false;
+        //     boolean b = false;
+        //     boolean answ = a || b ;
+        //     System.out.println(answ);
+
+        // Not logical Operator
+        // boolean y = true;
+        // boolean answer = !y;
+        // System.out.println(answer);
+     }
 }
