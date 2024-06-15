a0c942eb84cd7ead178db6b9cdc29d6d4d704787
arjun98k <arjunkandekar14@gmail.com>
2024-06-15 14:04:10 +0530

    first commit

---
+++ Logical_Operator.java
@@ -0,0 +1,8 @@
+public class Logical_Operator {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = true;
+        boolean answ = a && b ;
+        System.out.println(answ);
+    }
+}
