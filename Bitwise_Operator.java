16b404bdea98395100bff627bd3a346dc1df57d3
arjun98k <arjunkandekar14@gmail.com>
2024-06-16 09:44:34 +0530

    first commit

---
+++ Bitwise_Operator.java
@@ -0,0 +1,10 @@
+public class Bitwise_Operator {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 14;
+        int c = 19;
+      
+        System.out.println(a<b & a>c);
+        
+    }
+}
