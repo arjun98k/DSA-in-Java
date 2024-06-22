8a374c0a12a6ab5e1da47fa6cde42b5e2d1a4e0c
arjun98k <arjunkandekar14@gmail.com>
2024-06-23 01:54:25 +0530

    first commit

---
+++ Loop.java
@@ -0,0 +1,11 @@
+public class Loop {
+    public static void main(String[] args) {
+        int end = 99;
+        for (int i = 1; i < end; i++) {
+            System.out.println("arjun love shri forever"+" "+ i);
+            
+        }
+
+            
+        }
+    }
