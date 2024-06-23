a2b766c314311282ebcfcda88ea7aff41b86cd7a
arjun98k <arjunkandekar14@gmail.com>
2024-06-23 13:58:07 +0530

    first commit

---
+++ Stringular/String_method.java
@@ -0,0 +1,8 @@
+package Stringular;
+
+public class String_method {
+    public static void main(String[] args) {
+        String Ak = new String("Arjun forever divine of krishna");
+        System.out.println(Ak);
+    }
+}
