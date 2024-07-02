3c400c68aa3a496a6fea4f951ef7dee134027e1a
arjun98k <arjunkandekar14@gmail.com>
2024-07-02 13:47:02 +0530

    hava while loop

--- JAVA_1.java
+++ JAVA_1.java
@@ -1,6 +1,18 @@
 public class JAVA_1 {
     
-    void main(){
-        System.out.println("hello arjun without args ");
+   public static void main(String[] args) {
+    int index = 0;
+    int start = 5;
+    int end = 5;
+      while (index < 11) {
+         if (index == end) {
+             System.out.println("i like computer science");
+         }
+        else{
+          System.out.println("i dont  like computer science");
+        }
+         index++;
+      
     }
+   }
 }
