f64b863599631c22b5811f91e98f84d3a4637d98
arjun98k <arjunkandekar14@gmail.com>
2024-07-05 10:58:28 +0530

    java code

---
+++ LoOpss_for.java
@@ -0,0 +1,10 @@
+public class LoOpss_for {
+    public static void main(String[] args) {
+        for (int m = 0; m < 6; m++){
+            if (m == 4) {
+             break;
+            }System.out.println(m);
+         
+        }
+    }
+}
---
+++ do_while_Loopss.java
@@ -0,0 +1,8 @@
+public class do_while_Loopss {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.println("i love her forever for unknown reason");a++;
+        } while (a<4); 
+    }
+}
