d91d96d17c0aa6653bd7db114e16e4d1aa78218f
arjun98k <arjunkandekar14@gmail.com>
2024-07-16 23:59:02 +0530

    arrays

--- Star1.java
+++ Star1.java
@@ -2,5 +2,12 @@ public class Star1 {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        int end = 5;
+        for (int i = 0; i < end; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+                
+            }System.out.println("");
+        }
     }
 }
